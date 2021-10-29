bbc581fee912a92babf87a7fc8bd9aadf01f8034
jonathan-hdz-unosquare <jonathan.hernandez@unosquare.com>
2021-10-28 19:33:00 -0500

    giffexpert

---
+++ src/2.-gifExpertApp/components/AddCategory.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AddCategory = () => {
+    return (
+        <div>
+            <h2>add</h2>
+        </div>
+    )
+}
--- src/2.-gifExpertApp/components/GiffApp.js
+++ src/2.-gifExpertApp/components/GiffApp.js
@@ -1,9 +1,59 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import '../index.css'
+import { AddCategory } from './AddCategory';
 export const GiffApp = () => {
+    const [category, setCategory] = useState({});
+    const [categories, setCategories]= useState([])
+        const onChangeCategory=(e)=>{
+            const {target} = e;
+            setCategory({...category,[target.id]:target.value});
+        };
+        console.log(categories)
+
+        const addCategory=(e)=>{
+            e.preventDefault();
+            setCategories([...categories,category])
+        }
+
     return (
+      <div>
+        GiffExpert
+        <hr />
+        <AddCategory />
+        <form onSubmit={addCategory}>
         <div>
-            hello
+          <input
+            type="text"
+            id="category"
+            name="category"
+            placeholder="placeholder"
+            onChange={onChangeCategory}
+          />
         </div>
-    )
+        <div>
+          <input
+            type="text"
+            id="category2"
+            name="category2"
+            placeholder="find your giff"
+            onChange={onChangeCategory}
+          />
+        </div>
+        <div>
+            <input
+            type="checkbox"
+            id="age"
+            name="age"
+            onChange={onChangeCategory}
+            />
+        </div>
+        <input type="submit" name="addbutton" value="Add" />
+        </form>
+        <ul>
+          {
+            // categories.map((x,i)=><li key={i}>{x}</li>)
+          }
+        </ul>
+      </div>
+    );
 }
